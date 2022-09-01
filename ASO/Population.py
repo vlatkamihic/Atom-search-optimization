93f48fd3e689c1366ecd4435718f1b533efd9432
vlatkamihic <vlatka.mihic25@gmail.com>
2022-09-01 17:14:21 +0200

    Apply changes before daca

--- ASO/Population.py
+++ ASO/Population.py
@@ -13,125 +13,225 @@ def calcSumDis(pDim, kBest, i):
         sum += atom.position[i] - pDim
     return sum
 
+def calculateNorm(x_i, x_j):
+    sum_abs = 0
+    for i in x_i:
+        sum_abs += math.pow(i, 2)
+    for j in x_j:
+        sum_abs += math.pow(j, 2)
+
+    norm = math.sqrt(sum_abs)
+    return norm
+
+def euclideanDistance(x_i, x_j):
+    sum_abs = 0
+    n = len(x_i)
+    for i in range(n):
+        sum_abs += math.pow((x_i[i]-x_j[i]), 2)
+
+    dist = math.sqrt(sum_abs)
+    return dist
+
+def fromRange(value, min, max):
+    newValue = value
+    if value < min:
+        newValue = min
+    elif value > max:
+        newValue = max
+    else:
+        newValue = value
+    return newValue
+
+
 class Population(object):
 
     atoms = []
-    def __init__(self, pop_size):
+    def __init__(self, pop_size, f_min, f_max):
         self.pop_size = pop_size
-        self.fit_best = float(100)
-        self.fit_worst = 0
-        self.mass_sum = float(0.1)
+        self.f_min = f_min
+        self.f_max = f_max
+        #self.fit_best = sys.float_info.max
+        self.fit_best = float(sys.float_info.max)
+        self.fit_worst = float(0)
+        self.mi_sum = 1
         self.g_0 = 1.1
         self.u = 1.24
         
-
-    # using property decorator
-    # a getter function
-    def __getattribute__(self,name):
-        return object.__getattribute__(self, name)
     
 
-    def generatePopulation(self, minP, maxP, minV, maxV, d, depth, multiplierWeight):
+    def generatePopulation(self, d, depth, multiplierWeight):
         for i in range(self.pop_size):
             fitValue = sys.float_info.max
-            self.atoms.append(Atom(minP, maxP, minV, maxV, d, fitValue, depth, multiplierWeight))
-        #self.calculateMassSum()
+            self.atoms.append(Atom(self.f_min, self.f_max, d, fitValue, depth, multiplierWeight))
 
+    def evaluatePopulation(self, costFunction):
 
-    def calculateMassSum(self):
-        mass_sum = 0
+        for i, atom in enumerate(self.atoms):
+            pos = atom.position
+            #print(pos)
+            self.updateAtom(i, costFunction(pos))
+            #print("%s. iteration fitValue: %s" % (str(i), str(costFunction(pos))))
+
+    def calculateK(self, cur_iter, num_iter):
+        return self.pop_size - (self.pop_size - 2) * (math.sqrt(cur_iter / num_iter))
+
+    def calculateMiSum(self):
+        mi_sum = 0
         for atom in self.atoms:
-            mass_sum += atom.mass
-        #print(self.fit_worst)
-        #print("Mass sum: %s" % (str(mass_sum)))
-        self.mass_sum = mass_sum
-        return mass_sum
+            mi_sum += atom.mi
+        self.mi_sum = mi_sum
+        return mi_sum
     
-    def calculateKbest(self, curr_atom, cur_iter, num_iter):
-        newlist = self.atoms
+    def calculateKbest(self, cur_iter, num_iter):
+        newlist = []
+        for atom in self.atoms: newlist.append(atom)
         for i in range(len(newlist)):
             for j in range(len(newlist)-1):
                 if(newlist[i].fitValue > newlist[j].fitValue):
                     temp = newlist[i]
                     newlist[i] = newlist[j]
                     newlist[j] = temp
-        K = curr_atom.calculateK(cur_iter, num_iter)
+        K = self.calculateK(cur_iter, num_iter)
+        #print("K: "+str(K))
         kBest = []
-        for i in range(min(round(K), self.pop_size)):
+        for i in range(round(K)):
             kBest.append(newlist[i])
       
-        self.fit_best = newlist[0].fitValue
-        self.fit_worst = newlist[self.pop_size - 1].fitValue
+        #self.fit_best = newlist[0].fitValue
+        #self.fit_worst = newlist[self.pop_size - 1].fitValue
         return kBest
 
     def calculateDriftFactor(self, cur_iter, num_iter):
         return 0.1 * math.sin((math.pi / 2) * (cur_iter / num_iter))
 
-    def calculateAcceleration(self, alpha, beta, curr_atom, cur_iter, num_iter):
+    def calculateConstraintForce(self, curr_atom, best_atom, cur_iter, num_iter, beta, dimention):
         
-        depth = alpha * (math.pow((1-(cur_iter-1)/num_iter), 3)) * math.exp(-(20*cur_iter)/(num_iter))
-        lagrang = beta * math.exp(-(20*cur_iter)/num_iter)
+        lagrangian = beta * math.exp(-(20*cur_iter) / num_iter)
+        g_i = lagrangian * (best_atom.position[dimention] - curr_atom.position[dimention])
         
+        #print("Lagrangian: "+str(lagrangian))
+        #print("Constraint: "+str(g_i))
+        return g_i
+
+    def calculateDepthFactor(self, alfa, cur_iter, num_iter):
+        base = 1 - (cur_iter-1)/(num_iter)
+        eta = alfa * (math.pow(base, 3)) * math.exp(-(20*cur_iter)/num_iter)
+        return eta
+
+    def calculateLengthScale(self, x_i, K, kBest, j):
+        
+        x_ij = x_i[j]
+
+        
+        sum = 0
+        for k in kBest:
+            x_ij_temp = k.position[j]
+            sum += x_ij_temp
+        
+        secElement = sum / K
+        lengthScale = float(math.sqrt(math.pow(x_ij, 2)+math.pow(secElement, 2)))
+        if(lengthScale == 0):
+            lengthScale = 1
+        #print("Length scale(sigma): "+str(lengthScale))
+        return lengthScale
+
+
+    def calculateHij(self, curr_atom, atom, d, cur_iter, num_iter):
         h_min = self.g_0 + self.calculateDriftFactor(cur_iter, num_iter)
         h_max = self.u
+
+        r_ij = euclideanDistance(curr_atom.position, atom.position)
+        K = self.calculateK(cur_iter, num_iter)
+        kBest = self.calculateKbest(cur_iter, num_iter)
+
+        length_scale = self.calculateLengthScale(curr_atom.position, K, kBest, d)
+
+        h_ij = 0
+        if((r_ij/length_scale) < h_min):
+            h_ij = h_min
+        elif((r_ij/length_scale) > h_max):
+            h_ij = h_max
+        else:
+            h_ij = r_ij/length_scale
+        #print("h_ij: "+str(h_ij))
+        return h_ij
+
+    
+
+    def calculateForce(self, curr_atom, atom, d, cur_iter, num_iter):
         
-        kBest = self.calculateKbest(curr_atom, cur_iter, num_iter)
+        h_ij = self.calculateHij(curr_atom, atom, d, cur_iter, num_iter)
+        #m_i = curr_atom.calculateMass(self.mi_sum, self.fit_best, self.fit_worst)
+        m_i = curr_atom.mass
+        x_i = curr_atom.position
+        x_j = atom.position
+        euclidean_norm = calculateNorm(x_i, x_j)
+        norm = (x_j[d]-x_i[d])/(euclidean_norm)
+
+        f_ij = random.uniform(0,1) * ((2*math.pow(h_ij, 13)-math.pow(h_ij, 7))/m_i) * norm
+        #f_ij = random.choice([0,1]) * ((2*math.pow(h_ij, 13)-math.pow(h_ij, 7))/m_i) * norm
+        #f_ij = random.choice([0,1]) * ((2*math.pow(h_ij, 13)-math.pow(h_ij, 7))) * norm
         
-        f_i = numpy.zeros(curr_atom.dimention)
-        
-        pCurAtom = curr_atom.position
-        
+        return f_ij
+
+
+    def calculateInteractionForce(self, alfa, curr_atom, i, cur_iter, num_iter, dimention):
+
+        depth = self.calculateDepthFactor(alfa, cur_iter, num_iter)
+        kBest = self.calculateKbest(cur_iter, num_iter)
+        f_temp = 0
+        j = 0
         for atom in kBest:
-            i = 0
-            for pDim in atom.position:
-                dist = pDim - pCurAtom[i] # r_ij
-                sum_dis = calcSumDis(pDim, kBest, i)
-                
-                sigma = math.sqrt(math.pow(dist, 2) + math.pow((sum_dis/curr_atom.calculateK(cur_iter, num_iter)), 2))
-                r_sigma = dist / sigma
+            f_temp += self.calculateForce(curr_atom, atom, dimention, cur_iter, num_iter)
+            j += 1
+        f_i = -depth * f_temp
+        #print("Force(f_i): "+str(f_i))
+        return f_i
+            
 
-                h_ij = 0
-                if(r_sigma < h_min):
-                    h_ij = h_min
-                elif(r_sigma > h_max):
-                    h_ij = h_max
-                else:
-                    h_ij = r_sigma
-                
-                f_ij = -depth * (2 * math.pow(h_ij, 13) - math.pow(h_ij, 7))
-                #print(f_ij)
-                f_i[i] += random.uniform(0,1) * f_ij
-                i = i + 1
+    def calculateAcceleration(self, alpha, beta, curr_atom, i, best_atom, cur_iter, num_iter):
+        
+        acceleration = []
 
-        g_i = numpy.zeros(curr_atom.dimention)
-        pBest = kBest[0].position
-        acceleration = numpy.zeros(curr_atom.dimention)
-        i = 0
-        m_i = curr_atom.calculateMass(self.calculateMassSum(), self.fit_best, self.fit_worst)
+        #m_i = curr_atom.calculateMass(self.mi_sum, self.fit_best, self.fit_worst)
+        m_i = curr_atom.mass
 
-        for position in pCurAtom:
-            g_i[i] = lagrang * (pBest[i] - position)
+        for d in range(curr_atom.dimention):
+            
+            #print("Dimention--"+str(d)+"--")
+            f_i = self.calculateInteractionForce(alpha, curr_atom, i, cur_iter, num_iter, d)
+            g_i = self.calculateConstraintForce(curr_atom, best_atom, cur_iter, num_iter, beta, d)
+            
+            constraction = g_i / m_i
+            a = (f_i + constraction)
+            #print("Acceleration: "+str(a))
+            #print("-------------------------------------")
+            acceleration.append(fromRange(a, 0.1*self.f_min, 0.1*self.f_max))
+        
+        #print("F: %s, G: %s, a: %s" % (str(f_i), str(g_i), str(acceleration)))
 
-            acceleration[i] = (g_i[i] / m_i) + ( f_i[i] / m_i )
-
-            i = i + 1
-        #print(acceleration)
         return acceleration
 
-    def calculateVelocityAndPosition(self, alpha, beta, curr_atom, cur_iter, num_iter):
-        acceleration = self.calculateAcceleration(alpha, beta, curr_atom, cur_iter, num_iter)
-        velocity = curr_atom.velocity
+    def calculateVelocityAndPosition(self, alpha, beta, curr_atom, index,  best_atom, cur_iter, num_iter):
 
-        d = curr_atom.dimention
-        for i in range(d):
-            velocity[i] = random.uniform(0, 1) * velocity[i]  + acceleration[i]
+        acceleration = self.calculateAcceleration(alpha, beta, curr_atom, index, best_atom, cur_iter, num_iter)
+        #print("Acceleration: "+str(acceleration))
+        velocity = []
+        position = []
+
+        for d in range(curr_atom.dimention):
+            v = random.uniform(0,1) * curr_atom.velocity[d] + acceleration[d]
+            velocity.append(fromRange(v, 0.1*self.f_min, 0.1*self.f_max))
+            position.append(curr_atom.position[d] + velocity[d])
         
-        position = curr_atom.position
+        #curr_atom.updateParams(position, velocity)
 
-        for i in range(d):
-            position[i] = position[i] + velocity[i]
 
-        curr_atom.updateParams(position, velocity)
+        return position, velocity
+
+    def calculateMassForEachAtom(self):
+        for atom in self.atoms:
+            atom.calculateMass(self.mi_sum, self.fit_best, self.fit_worst)
 
     def updateAtom(self, index, fitValue):
         self.atoms[index].fitValue = fitValue
@@ -139,10 +239,38 @@ class Population(object):
     def getFitValues(self):
         gFits = []
         for atom in self.atoms:
+            #print("GetFITS fit values: "+str(atom.fitValue))
             gFits.append(atom.fitValue)
-
+        print(gFits)
         return gFits
 
-    def getAtom(self, index):
-        list = Population.atoms
-        return list[index]
+    def getFitWorst(self):
+
+        for atom in self.atoms:
+            if(atom.fitValue > self.fit_worst):
+                self.fit_worst = atom.fitValue
+        #print("FIT WORST: "+str(self.fit_worst))
+        return self.fit_worst
+    
+    def getFitBest(self):
+
+        for atom in self.atoms:
+            if(atom.fitValue < self.fit_best):
+                self.fit_best = atom.fitValue
+        #print("FIT BEST: "+str(self.fit_best))
+        return self.fit_best
+    
+    def getBestInd(self):
+        best_ind = self.atoms[0]
+        for atom in self.atoms:
+            if(atom.fitValue == self.fit_best):
+                best_ind = atom
+        #print("FIT BEST: "+str(self.fit_best))
+        return best_ind
+
+
+    def updatePopulation(self):
+        self.calculateMiSum()
+        self.calculateMassForEachAtom()
+        self.getFitWorst()
+        self.getFitBest()
--- ASO/atom.py
+++ ASO/atom.py
@@ -6,59 +6,52 @@ import sys
 
 class Atom(object):
     
-    def __init__(self, minP, maxP, minV, maxV, d, fitValue, depth, multiplierWeight):
+    def __init__(self, minP, maxP, d, fitValue, depth, multiplierWeight):
 
         self.dimention = d
         self.depthWeight = depth
         self.multiplierWeight = multiplierWeight
         self.fitValue = fitValue
-        self.mass = 0.0000001
+        self.mass = float(0.0000001)
+        self.mi = float(0.001)
         self.position = list(numpy.random.uniform(minP, maxP, size=(d)))
-        #print(self.position)
-        #self.velocity = list(numpy.random.uniform(minV, maxV, size=(d)))
         self.velocity = [0] * d
-
-    def getPosition(self):
-        return self.position
-    #def __repr__(self):
-     #   return str((self.dimention, self.depthWeight, self.multiplierWeight, self.fitValue, self.mass, self.position, self.velocity))
 
     def updateAtom(self, new):
         self.fitValue = new.fitValue
         self.mass = new.mass
-        print("Old position: %s, new position: %s" % (str(self.fitValue), str(new.fitValue)))
+        self.mi = new.mi
+        #print("Old position: %s, new position: %s" % (str(self.fitValue), str(new.fitValue)))
         for i in range(self.dimention):
 
             self.position[i] = new.position[i]
             self.velocity[i] = new.velocity[i]
     
     def updateParams(self, position, velocity):
+        #print("Old position: %s, new position: %s, fit: %s" % (str(self.position), str(position), str(self.fitValue)))
         for i in range(self.dimention):
 
             self.position[i] = position[i]
             self.velocity[i] = velocity[i]
-
-    def updateFitValue(self, fitValue):
-        self.fitValue = fitValue
     
     def calculateMi(self, fitWorse, fitBest):
         #print("FitValue %s, fitBest %s, fitWorst%s" % (str(self.fitValue), str(fitBest), str(fitWorse)))
-        if(fitBest > 99999 or fitWorse == fitBest):
-            mi = sys.float_info.max
-        else:
-            try:
+        
+        try:
+            if(fitWorse == fitBest):
+                mi = math.exp(-(self.fitValue-fitBest)/(1))
+            #mi = 100
+            else:
                 mi = math.exp(-(self.fitValue-fitBest)/(fitWorse-fitBest))
-            except OverflowError:
-                mi = sys.float_info.max
+        except OverflowError:
+            mi = sys.float_info.max
+                #mi = 100
         #print("MI: %s" % (str(mi)))
+        self.mi = mi
         return mi
     
-    def calculateMass(self, mass_sum, fitBest, fitWorse):
+    def calculateMass(self, mi_sum, fitBest, fitWorse):
         mi = self.calculateMi(fitWorse, fitBest)
-        #print("MI: %s" % (str(mi)))
-        self.mass = mi / mass_sum
+        #print("Best: %s Worst: %s MI: %s MI_SUM: %s" % (str(fitBest), str(fitWorse), str(mi), str(mi_sum)))
+        self.mass = mi / mi_sum
         return self.mass
-
-    def calculateK(self, cur_iter, num_iter):
-        return self.dimention - (self.dimention - 2) * (math.sqrt(cur_iter / num_iter))
-
